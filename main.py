3cb06955c03ea7a486247978738f33edc71fcc7b
erikvim <erikdavid.hedman21@outlook.com>
2024-11-03 21:08:13 -0500

    Created three new functions

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+def count_characters(text):
+    
+    diction = {}
+    lowered = text.lower()
+    for char in lowered: 
+        if char in diction: 
+            diction[char] += 1
+        else: 
+            diction[char] = 1    
+    print(diction) 
+
+def count_words(text):
+    words = text.split()
+    # print(words)
+    count_of_words = len(words)
+    return count_of_words
 def main():
     with open('books/genesis.txt', 'r') as f:
         text = f.read()
-    print(text)
-    
-main()    
+    # print(text)
+    print(count_words(text))
+    count_characters(text)
+
+
+main()
