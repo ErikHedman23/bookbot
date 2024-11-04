a570cc433fbffe85292ed77fde33caadb767397d
erikvim <erikdavid.hedman21@outlook.com>
2024-11-03 21:15:20 -0500

    Added a report feature to the count_characters fn, and added a pause to each result printed to the console

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+
 def count_characters(text):
-    
     diction = {}
     lowered = text.lower()
     for char in lowered: 
@@ -7,19 +8,23 @@ def count_characters(text):
             diction[char] += 1
         else: 
             diction[char] = 1    
-    print(diction) 
-
+    # print(diction)
+     
+    for key in diction:
+        print(f"The {key} character was found {diction[key]} times...")
+        time.sleep(0.1)
+     
 def count_words(text):
     words = text.split()
     # print(words)
     count_of_words = len(words)
     return count_of_words
+
 def main():
     with open('books/genesis.txt', 'r') as f:
         text = f.read()
     # print(text)
     print(count_words(text))
     count_characters(text)
-
 
 main()
